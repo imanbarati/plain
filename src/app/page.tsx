0011dacf6963dc9c29098f92473c3eabb1176eb6
Iman Manati <40170427+imanbarati@users.noreply.github.com>
2025-04-12 17:26:59 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+ <meta name="enamad" content="49208935" /> 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
